cbe5078efec564f22219184fa9b56510f4bc8384
latitudu.latitudu@gmail.com <latitudu.latitudu@gmail.com@6f26db81-20c1-779e-3ef7-29c585eff798>
2013-12-21 07:09:50 +0000

    Edit.

--- full-screen.js
+++ full-screen.js
@@ -9,12 +9,11 @@ window.postFullScreen = function(config) {
 		padding: 50,
 		maxWidth: 750,
 		createButton: document.createElement('button'),
-		buttonText: "Full Screen Mode",
+		openText: "Full Screen Mode",
 		closeText: "Exit full screen mode",
 		appendButtonTo: null,
 		beforeInit: null,
-		afterInit: null,
-		scrollableContent: document.body
+		afterInit: null
 	};
 	for (var i in defaults) {
 		defaults[i] = (typeof config[i] != 'undefined') ? config[i] : defaults[i];
@@ -24,17 +23,17 @@ window.postFullScreen = function(config) {
 		defaults.beforeInit();
 	}
 	div.innerHTML = '<div style="position:fixed!important;position:absolute;top:0;right:0;bottom:0;left:0;z-index:999999;background:' + defaults.background + ';color:' + defaults.color + ';padding:' + defaults.padding + 'px;padding-bottom:0;font-size:' + defaults.fontSize + ';overflow:auto;letter-spacing:0;text-transform:none;text-shadow:none;display:none;"><div style="max-width:' + defaults.maxWidth + 'px;margin:0 auto ' + defaults.padding + 'px;"><h1 style="font:inherit;color:inherit;margin:0;padding:0;font-size:300%;font-weight:bold;text-transform:none;letter-spacing:0;text-shadow:none;">' + defaults.titleSource.innerHTML + '</h1>' + defaults.contentSource.innerHTML + '</div><div style="text-align:center;margin:' + defaults.padding + 'px 0;"><button style="display:inline;display:inline-block;" onclick="this.parentNode.parentNode.style.display=\'none\';document.body.style.overflow=\'\';return false;">' + defaults.closeText + '</button></div><a href="#exit" style="display:block;position:absolute;top:18px;right:20px;font:normal bold 30px/30px Arial,Sans-Serif;color:inherit;text-decoration:none;overflow:hidden;background:none;opacity:.3;" onclick="this.parentNode.style.display=\'none\';document.body.style.overflow=\'\';return false;" onmousedown="this.style.opacity=0.8;" onmouseup="this.style.opacity=0.4;" onmouseover="this.style.opacity=0.5;" onmouseout="this.style.opacity=0.4;" title="' + defaults.closeText + '">&times;</a></div>';
-	btn.innerHTML = defaults.buttonText;
+	btn.innerHTML = defaults.openText;
 	btn.onclick = function() {
 		div.children[0].style.display = "block";
-		defaults.scrollableContent.style.overflow = "hidden";
+		document.body.style.overflow = "hidden";
 		return false;
 	};
 	defaults.contentSource.appendChild(div);
 	if (defaults.contentSource) {
 		if (defaults.appendButtonTo !== false) defaults[defaults.appendButtonTo === null ? "contentSource" : "appendButtonTo"].appendChild(btn);
 	} else {
-		defaults.scrollableContent.appendChild(btn);
+		document.body.appendChild(btn);
 	}
 	if (typeof defaults.afterInit == 'function') {
 		defaults.beforeInit();
@@ -43,6 +42,6 @@ window.postFullScreen = function(config) {
 	// auto open full screen mode when there is a `#full-screen` hash at the end of the URL
 	if (window.location.hash && window.location.hash.replace('#', "") == 'full-screen') {
 		div.children[0].style.display = "block";
-		defaults.scrollableContent.style.overflow = "hidden";
+		document.body.style.overflow = "hidden";
 	}
 };
